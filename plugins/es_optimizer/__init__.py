3921a91692101aa50118f459944daf79cf8c6c53
Philipp Wundrack <philipp.wundrack@live.de>
2022-02-28 10:27:55 +0100

    fix endpoints not being imported

--- plugins/es_optimizer/__init__.py
+++ plugins/es_optimizer/__init__.py
@@ -1,2 +1,2 @@
 from .plugin import __version__ # import plugin and version constant
-from . import evolutionary_strategy # must happen last to avoid import problems
+from .endpoints import rank, learn_ranking # must happen last to avoid import problems
