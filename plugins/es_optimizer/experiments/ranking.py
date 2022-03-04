e1cf0f28be6da41b84731c0347683a705bceb6b7
Philipp Wundrack <philipp.wundrack@live.de>
2022-03-04 13:34:25 +0100

    add ranking helper functions

---
+++ plugins/es_optimizer/experiments/ranking.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+def convert_scores_to_ranking(scores: np.ndarray, higher_scores_is_better: bool) -> np.ndarray:
+    if higher_scores_is_better:
+        scores = -scores
+
+    return np.argsort(np.argsort(scores))
+
+
+def sort_array_with_ranking(array: np.ndarray, ranking: np.ndarray) -> np.ndarray:
+    return array[np.argsort(ranking)]
+
+
+def _test():
+    test = np.array([3, 4, 5, 2])
+    ranking = convert_scores_to_ranking(test, False)
+    test_sorted = sort_array_with_ranking(test, ranking)
+    print(ranking)
+    print(test_sorted)
+
+
+if __name__ == "__main__":
+    _test()
