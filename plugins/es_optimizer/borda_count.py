fix rank calculation

plugins/es_optimizer/borda_count.py
@@ -2,15 +2,17 @@ from typing import List
 
 import numpy as np
 
+from plugins.es_optimizer.experiments.ranking import convert_scores_to_ranking
 
-def borda_count(rankings: List[np.ndarray]) -> np.ndarray:
+
+def borda_count_rank(rankings: List[np.ndarray]) -> np.ndarray:
     points = np.zeros_like(rankings[0])
     size = rankings[0].shape[0]
 
     for ranking in rankings:
         points += size - ranking - 1
 
-    return np.argsort(-points)
+    return convert_scores_to_ranking(points, True)
 
 
 def _test():
@@ -18,7 +20,7 @@ def _test():
     rank2 = np.array([0, 1, 2, 3])
     rank3 = np.array([3, 0, 1, 2])
 
-    print(borda_count([rank1, rank2, rank3]))
+    print(borda_count_rank([rank1, rank2, rank3]))
 
 
 if __name__ == "__main__":

plugins/es_optimizer/endpoints/sensitivity.py
@@ -18,6 +18,7 @@ from qhana_plugin_runner.storage import STORE
 from qhana_plugin_runner.tasks import save_task_result, save_task_error
 
 from plugins.es_optimizer.api import PLUGIN_BLP, RankSensitivitySchema
+from plugins.es_optimizer.experiments.ranking import convert_scores_to_ranking
 from plugins.es_optimizer.parsing import get_metrics_from_compiled_circuits, parse_metric_info
 from plugins.es_optimizer.plugin import EsOptimizer
 
@@ -92,7 +93,7 @@ def rank_sensitivity_task(self, db_id: int) -> str:
     else:
         raise ValueError("Unknown method: " + str(task_parameters["method"]))
 
-    original_ranking = np.argsort(-mcda(metrics, weights, is_cost))
+    original_ranking = convert_scores_to_ranking(mcda(metrics, weights, is_cost), True)
     unitary_variation_ratios = task_parameters["unitary_variation_ratios"]
 
     output_data = {
@@ -118,7 +119,7 @@ def rank_sensitivity_task(self, db_id: int) -> str:
             disturbed_weights /= np.sum(disturbed_weights)
             new_result["disturbed_weights"].append(disturbed_weights.tolist())
 
-            disturbed_ranking = np.argsort(-mcda(metrics, disturbed_weights, is_cost))
+            disturbed_ranking = convert_scores_to_ranking(mcda(metrics, disturbed_weights, is_cost), True)
             new_result["disturbed_rankings"].append(disturbed_ranking.tolist())
 
         output_data["results"].append(new_result)

plugins/es_optimizer/experiments/ranking.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 
-def convert_scores_to_ranking(scores: np.ndarray, higher_scores_is_better: bool) -> np.ndarray:
-    if higher_scores_is_better:
+def convert_scores_to_ranking(scores: np.ndarray, higher_scores_are_better: bool) -> np.ndarray:
+    if higher_scores_are_better:
         scores = -scores
 
     return np.argsort(np.argsort(scores))